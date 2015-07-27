a2267a0e11404eb9ee370384720b9a0afea01b2c
Gabriel Poça <gabrielpoca@gmail.com>
2015-07-27 18:56:54 +0100

    Add sending email test for the applications package

--- packages/apprenticeship-applications/applications_test.js
+++ packages/apprenticeship-applications/applications_test.js
@@ -1,5 +1,34 @@
 describe('Applications', function() {
+  beforeEach(function() {
+    Applications.remove({});
+    spies.restoreAll();
+    Meteor.users.remove({});
+  });
+
   it('exists', function() {
     expect(Applications).to.be.an('object');
+  });
+
+  Meteor.startup(function() {
+    it('sends an email when an application is submited', function() {
+      var emailSpy = sinon.spy(Email, 'send');
+
+      Accounts.createUser({
+        username: 'gabrielpoca',
+        password: 'random',
+        profile: {
+          name: 'Gabriel',
+          email: 'gabriel@subvisual.co',
+        }
+      });
+
+      Applications.insert({
+        name: 'Gabriel Poça',
+        email: 'gabriel@subvisual.co',
+        phoneNumber: 123123123
+      });
+
+      expect(emailSpy).to.have.been.called;
+    });
   });
 });
--- packages/apprenticeship-applications/package.js
+++ packages/apprenticeship-applications/package.js
@@ -10,11 +10,14 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
   api.use('email');
+  api.use('accounts-base');
   api.use('aldeed:autoform@4.0.0 || 5.0.0');
   api.use('matb33:collection-hooks@0.7.5');
   api.use('gabrielpoca:state-machine');
   api.use('aldeed:collection2');
   api.use('grigio:babel');
+
+  api.imply(['email', 'accounts-base']);
 
   api.addFiles('applications.js');
   api.addFiles('server_hooks.es6', 'server');
@@ -26,9 +29,14 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use([
     'mike:mocha-package@0.5.7',
-    'practicalmeteor:chai@2.1.0_1'
+    'practicalmeteor:chai@2.1.0_1',
+    'practicalmeteor:sinon@1.14.1_2'
   ]);
 
+  // I'm including this package to be able to create accounts manually.
+  // I wouldn't be able to test because of the headquarters integrations,
+  // unless I had a fake server.
+  api.use('accounts-password');
   api.use('apprenticeship:applications');
 
   api.addFiles([
