91c9f25c8d675d319849a55ec542bed6cb7319a2
Gabriel Poça <gabrielpoca@gmail.com>
2015-07-31 12:53:07 +0100

    Remove customLogin in favor loginButtons

--- packages/apprenticeship-theme/theme.js
+++ packages/apprenticeship-theme/theme.js
@@ -3,22 +3,3 @@ Template.menuLogoutButton.events({
     Meteor.logout();
   }
 });
-
-
-Template.customLogin.events({
-  'click button': function() {
-    var serviceName = 'headquarters';
-
-    var options = {};
-    if (Accounts.ui._options.requestPermissions[serviceName])
-      options.requestPermissions = Accounts.ui._options.requestPermissions[serviceName];
-    if (Accounts.ui._options.requestOfflineToken[serviceName])
-      options.requestOfflineToken = Accounts.ui._options.requestOfflineToken[serviceName];
-    if (Accounts.ui._options.forceApprovalPrompt[serviceName])
-      options.forceApprovalPrompt = Accounts.ui._options.forceApprovalPrompt[serviceName];
-
-    Meteor.loginWithHeadquarters(options, function (err) {
-      console.log('here');
-    });
-  }
-});
